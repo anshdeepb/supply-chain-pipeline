d40a16938b06ff5f8e6b6a24d40cdcf10b3203b7
Anshdeep Bhachoo <anshdeepbhachoo@d2:8c:d8:f1:5b:c8.home>
2026-07-01 16:12:24 -0400

    add the code to deploy the required s3 buckets

--- supply_chain_pipeline/supply_chain_pipeline_stack.py
+++ supply_chain_pipeline/supply_chain_pipeline_stack.py
@@ -1,19 +1,41 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
+from aws_cdk import Stack, RemovalPolicy
+from aws_cdk import aws_s3 as s3
+
+from aws_cdk import aws_glue as glue
+from aws_cdk import aws_iam as iam
+
 from constructs import Construct
 
 class SupplyChainPipelineStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, **kwargs):
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "SupplyChainPipelineQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        bronze = s3.Bucket(self, "Bronze",
+                            encryption=s3.BucketEncryption.S3_MANAGED,
+                            removal_policy=RemovalPolicy.RETAIN,
+                            versioned=True,
+                            block_public_access=s3.BlockPublicAccess.BLOCK_ALL
+                            )
+        
+        silver = s3.Bucket(self, "Silver",
+                           encryption=s3.BucketEncryption.S3_MANAGED,
+                           removal_policy=RemovalPolicy.RETAIN,
+                           versioned=True,
+                           block_public_access=s3.BlockPublicAccess.BLOCK_ALL
+                           )
+        
+        model = s3.Bucket(self, "Model",
+                          encryption=s3.BucketEncryption.S3_MANAGED,
+                          removal_policy=RemovalPolicy.RETAIN,
+                          versioned=True,
+                          block_public_access=s3.BlockPublicAccess.BLOCK_ALL
+                          )
+        
+        scripts = s3.Bucket(self, "Scripts",
+                          encryption=s3.BucketEncryption.S3_MANAGED,
+                          removal_policy=RemovalPolicy.RETAIN,
+                          versioned=True,
+                          block_public_access=s3.BlockPublicAccess.BLOCK_ALL
+                          )
+            
